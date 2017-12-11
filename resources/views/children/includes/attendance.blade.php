a51ae01b75a93552ed5799543200d8dbb51564e1
jgmuchiri <jgmuchiri@gmail.com>
2017-12-11 23:13:26 +0300

    Integrated child's attendance page

---
+++ resources/views/children/includes/attendance.blade.php
@@ -0,0 +1,43 @@
+<div class="content-heading">
+   <!-- START Language list-->
+   <div class="pull-right">
+      <div class="btn-group">
+         <button class="btn btn-success waves-effect m-b-5" data-toggle="modal" data-target="" data-backdrop="false"> <i class="fa fa-print m-r-5 btn-fa"></i> <span> @lang('Print')</span></button>
+      </div>
+   </div>
+   <h3 style="margin-top: 0px;">Attendance</h3>
+</div>
+<hr>
+<div class="panel panel-default">
+   <div class="panel-body">
+   		<!-- START table-responsive-->
+            <div class="table-responsive">
+               <table class="table table-striped table-bordered table-hover">
+                  <thead>
+                     <tr>
+                        <th>#</th>
+                        <th>Date</th>
+                        <th>Time in</th>
+                        <th>Time Out</th>
+                        <th>Pick up by</th>
+                        <th>Late Fee?</th>
+                     </tr>
+                  </thead>
+                  <tbody>
+                     <tr>
+                        <td>1</td>
+                        <td>10 October 2017</td>
+                        <td>10:00 AM</td>
+                        <td>05:00 PM</td>
+                        <td>Jamie Jones</td>
+                        <td>No</td>
+                     </tr>
+                  </tbody>
+               </table>
+            </div>
+            <!-- END table-responsive-->
+   </div>
+</div>
+
+
+
