4a7606838f6aeba7b4c6d318f764d572df12c81f
jgmuchiri <jgmuchiri@gmail.com>
2017-12-12 00:00:53 +0300

    Integrated child's notes page

---
+++ resources/views/children/includes/notes.blade.php
@@ -0,0 +1,38 @@
+<div class="content-heading">
+   <!-- START Language list-->
+   <div class="pull-right">
+      <div class="btn-group">
+         <button class="btn btn-success waves-effect m-b-5" data-toggle="modal" data-target="" data-backdrop="false"> <i class="fa fa-plus m-r-5 btn-fa"></i> <span> @lang('New Note')</span></button>
+      </div>
+   </div>
+   <h3 style="margin-top: 0px;">Notes</h3>
+</div>
+<hr>
+<div class="panel panel-default">
+   <div class="panel-heading">
+       <div class="row">
+		 <div class="col-lg-4">
+		    <div class="input-group">
+		       <input class="input-sm form-control" type="text" placeholder="Search">
+		       <span class="input-group-btn">
+		          <button class="btn btn-sm btn-default" type="button">Search</button>
+		       </span>
+		    </div>
+		 </div>
+		 <div class="col-lg-8"></div>
+	   </div>
+   </div>
+   <div class="panel-body">
+	<div class="media-box-body">
+	  <h4 class="media-box-heading general">10 October 2017 | General</h4>
+	  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus suscipit eros elit, eget molestie turpis ultricies et. Nullam non ornare ligula, a venenatis urna. Vestibulum sodales egestas massa, quis laoreet arcu. Integer euismod enim sit amet metus iaculis tincidunt. Suspendisse sit amet mi velit. Integer eu commodo nunc. Integer vitae tortor nisl. Ut porttitor arcu a mauris auctor, in consectetur dolor hendrerit.Cras massa diam, vehicula at ipsum eget, lobortis malesuada mauris. In hac habitasse platea dictumst. Aenean consectetur posuere consequat. Curabitur in purus sed ex blandit tincidunt at nec eros. Curabitur ligula sapien, convallis eu elementum quis, auctor ac urna. Aliquam commodo fermentum tristique. Quisque placerat ornare sodales. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sodales viverra tellus, sollicitudin pharetra purus efficitur eu. Etiam ullamcorper quam nec ante tempus sagittis. Nullam hendrerit fermentum massa. Aenean sed ultrices purus. In porttitor vulputate eros sit amet ornare.</p>
+	</div>
+	<div class="media-box-body">
+	  <h4 class="media-box-heading incident">9 October 2017 | Incedent</h4>
+	  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus suscipit eros elit, eget molestie turpis ultricies et. Nullam non ornare ligula, a venenatis urna. Vestibulum sodales egestas massa, quis laoreet arcu. Integer euismod enim sit amet metus iaculis tincidunt. Suspendisse sit amet mi velit. Integer eu commodo nunc. Integer vitae tortor nisl. Ut porttitor arcu a mauris auctor, in consectetur dolor hendrerit.Cras massa diam, vehicula at ipsum eget, lobortis malesuada mauris. In hac habitasse platea dictumst. Aenean consectetur posuere consequat. Curabitur in purus sed ex blandit tincidunt at nec eros. Curabitur ligula sapien, convallis eu elementum quis, auctor ac urna. Aliquam commodo fermentum tristique. Quisque placerat ornare sodales. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sodales viverra tellus, sollicitudin pharetra purus efficitur eu. Etiam ullamcorper quam nec ante tempus sagittis. Nullam hendrerit fermentum massa. Aenean sed ultrices purus. In porttitor vulputate eros sit amet ornare.</p>
+	</div>
+</div>
+</div>
+
+
+
