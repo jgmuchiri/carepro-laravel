79a947e4c91097040d7cc36b98a082c4508f4f78
Kevin Hicks <kevin@infinia.io>
2018-03-17 11:34:10 -0400

    Add missing translation strings

--- resources/views/partials/demo-warning.blade.php
+++
@@ -1,5 +0,0 @@
-@if(env('DEMO_MODE'))
-    <div class="callout callout-danger"><h4>Demo mode warning!</h4>
-        @lang('This application is in demo mode but any transactions are in real time. If you make any real transaction, no refunds will be issued. If you need to test the system, consider using very small amounts.')
-    </div>
-@endif
