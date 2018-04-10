f2c429ebea6486bf30700955a47a88b4c75a5137
jgmuchiri <jgmuchiri@gmail.com>
2018-04-10 16:16:34 +0300

    implement dropzone in child images upload

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -10,6 +10,7 @@ window._ = require('lodash');
 window.$ = window.jQuery = require('jquery');
 
 require('bootstrap-sass');
+import 'vue2-dropzone/dist/vue2Dropzone.css'
 
 /**
  * Vue is a modern JavaScript library for building interactive web interfaces
