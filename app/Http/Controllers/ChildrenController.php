48a5a12bf108479388c2c2ad895ec2a98ec47fd8
jgmuchiri <jgmuchiri@gmail.com>
2018-04-10 14:25:51 +0300

    change image size

--- app/Http/Controllers/ChildrenController.php
+++ app/Http/Controllers/ChildrenController.php
@@ -169,7 +169,7 @@ class ChildrenController extends Controller
         //retrieve the image
         $file = Storage::get('public/children-images/original/'.$photo_name);
         //resize image
-        $photo_thumb = Image::make($file)->resize(100, 100)->stream();
+        $photo_thumb = Image::make($file)->resize(200, 200)->stream();
         //move the resized image to the childrens folder.
         $path = Storage::disk('public')->put('children-images/'.$photo_name, $photo_thumb);
         //generate resized image path
@@ -248,7 +248,7 @@ class ChildrenController extends Controller
             //retrieve the image
             $file = Storage::get('public/children-images/original/' . $photo_uri);
             //resize image
-            $photo_thumb = Image::make($file)->resize(100, 100)->stream();
+            $photo_thumb = Image::make($file)->resize(200, 200)->stream();
             //move the resized image to the childrens folder.
             $path = Storage::disk('public')->put($photo_uri, $photo_thumb);
         }
