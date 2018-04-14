ecd711ee10956f2932237f1e007a217e8976168b
jgmuchiri <jgmuchiri@gmail.com>
2018-04-14 22:27:03 +0300

    Added allergy and medication routes

--- routes/api.php
+++ routes/api.php
@@ -43,6 +43,13 @@ Route::group(['middleware' => ['auth:api', 'subscribed']], function() {
     Route::resource('staff', 'StaffController', ['only' => ['index', 'store', 'update', 'edit']]);
 
     Route::get('children/{id}/attendance', 'AttendanceController@index');
+    //medication
     Route::get('children/{id}/medication', 'MedicationController@index');
     Route::post('children/{id}/medication', 'MedicationController@store');
+    Route::patch('children/{id}/medication/{medication_id}', 'MedicationController@update');
+    Route::delete('children/{id}/medication/{medication_id}', 'MedicationController@destroy');
+    //allergies
+    Route::post('children/{id}/allergy', 'AllergyController@store');
+    Route::patch('children/{id}/allergy/{allergy_id}', 'AllergyController@update');
+    Route::delete('children/{id}/allergy/{allergy_id}', 'AllergyController@destroy');
 });
