8462bb06b03eaef9c29c3e57e356fd0e15f8d279
jgmuchiri <jgmuchiri@gmail.com>
2018-04-14 15:27:32 +0300

    Added medication model, migration and controller

---
+++ app/Http/Controllers/MedicationController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Medication;
+
+class MedicationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index($id)
+    {
+        $medication = Medication::where('child_id', $id)->latest()->get();
+
+        return $medication;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, $id)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:255',
+            'frequency' => 'required',
+            'start' => 'required|date',
+            'stop' => 'required|date',
+        ]);
+
+        $medication = new Medication;
+        $medication->fill($request->all());
+        $medication->child_id = $id;
+        $medication->save();
+
+        return response()->json([
+            'status' => 'okay',
+            'status_code' => '200',
+            'message' => 'Medical record has been created',
+            'medication' => $medication
+        ], 200);
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ database/migrations/2018_04_12_093123_create_medications_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMedicationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('medications', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('child_id');
+            $table->foreign('child_id')->references('id')->on('children');
+            $table->string('name');
+            $table->string('frequency');
+            $table->date('start');
+            $table->date('stop');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('medications');
+    }
+}
