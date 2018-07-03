7a8822d6d20bde9abe6bc9931d4a5d91f579f9e0
jgmuchiri <jgmuchiri@gmail.com>
2018-07-02 20:29:21 -0400

    update vendor email template

--- resources/views/vendor/mail/html/footer.blade.php
+++ resources/views/vendor/mail/html/footer.blade.php
@@ -1,11 +1,22 @@
 <tr>
-    <td>
-        <table class="footer" align="center" width="570" cellpadding="0" cellspacing="0">
-            <tr>
-                <td class="content-cell" align="center">
-                    {{ Illuminate\Mail\Markdown::parse($slot) }}
-                </td>
-            </tr>
-        </table>
-    </td>
+	<td>
+		<table class="footer" align="center" width="570" cellpadding="0" cellspacing="0">
+			<tr>
+				<td class="content-cell" align="center">
+					{{ Illuminate\Mail\Markdown::parse($slot) }}
+				</td>
+			</tr>
+		</table>
+	</td>
+</tr>
+<tr>
+	<td class="content-block">
+		<span class="apple-link">{{ \Config::get('app.name') }}</span>
+		<br>
+	</td>
+</tr>
+<tr>
+	<td class="content-block powered-by">
+		{{ URL()->to('/') }}
+	</td>
 </tr>
